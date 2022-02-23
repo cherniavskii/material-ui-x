b2272e31de0b29501ab77e5e8c32d6f66fef9e8e
Andrew Cherniavskyi <andrew@mui.com>
2022-02-23 12:10:32 +0100

    add jsdoc

--- packages/grid/x-data-grid/src/internals/models/api/gridColumnSpanning.ts
+++ packages/grid/x-data-grid/src/internals/models/api/gridColumnSpanning.ts
@@ -7,12 +7,24 @@ import { GridRowId } from '../gridRows';
  * The Column Spanning API interface that is available in the grid `apiRef`.
  */
 export interface GridColumnSpanning {
-  // TODO
+  /**
+   * Returns the cell metadata with colSpan info.
+   * @param {GridRowId} rowId The row id
+   * @param {number} columnIndex The column index (0-based)
+   * @returns {GridCellMeta|undefined} Cell metadata
+   */
   unstable_getCellMeta: (
     rowId: GridRowId,
     columnIndex: GridColumnIndex,
   ) => GridCellMeta | undefined;
-  unstable_calculateColSpan: (params: {
+  /**
+   * Calculate column spanning for each cell in the row
+   * @param {Object} options The options to apply on the calculation.
+   * @param {GridRowId} options.rowId The row id
+   * @param {number} options.minFirstColumn First visible column index
+   * @param {number} options.maxLastColumn Last visible column index
+   */
+  unstable_calculateColSpan: (options: {
     rowId: GridRowId;
     minFirstColumn: number;
     maxLastColumn: number;
